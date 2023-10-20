a4805c111b035dce60d20041e588d51973c36a65
Vitalii Bielimov <vbit2000@gmail.com>
2023-10-20 18:13:11 +0300

    changed username

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='clean_folder',
     version='0.0.1',
     description='A script for sorting files in a specified directory',
-    url='http://github.com',
+    url='https://github.com/VBit7/clean_folder.git',
     author='Vitalii B.',
     author_email='vbit2000@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['clean-folder = clean_folder.main:main']}
+    entry_points={'console_scripts': ['clean-folder = clean_folder_vbit7.main:main']}
 )
