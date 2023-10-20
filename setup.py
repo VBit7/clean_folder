63ecc26ffc40167e90cb4f9def23963394790ae0
Vitalii Bielimov <vbit2000@gmail.com>
2023-10-20 19:06:34 +0300

    change setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_namespace_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='clean_folder_vbit7',
-    version='0.0.4',
+    version='0.0.5',
     description='A script for sorting files in a specified directory',
     url='https://github.com/VBit7/clean_folder.git',
     author='Vitalii B.',
     author_email='vbit2000@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
     license='MIT',
     classifiers = [
         "Programming Language :: Python :: 3",
